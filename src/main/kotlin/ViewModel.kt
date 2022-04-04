39b8e49ace03035bd6fd7c08cccc1178b68f9eec
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-04 13:52:04 +1000

    .

--- src/main/kotlin/ViewModel.kt
+++ src/main/kotlin/ViewModel.kt
@@ -14,14 +14,42 @@ class ViewModel {
 
   fun addLetter(letter: Char) {
     println("addLetter $letter")
+    val ws = wordleStateFlow.value
 
+    // word being edited
+    val word = ws.wordList[ws.cursorRow]
+
+    var newWord = word
+//    var newCursorRow = ws.cursorRow
+
+    when {
+      word.length < 5 -> {
+        newWord += letter
+      }
+      else -> {
+        return
+      }
+//      word.length == 4 -> {
+//        newWord += letter
+//        newCursorRow++
+//      }
+    }
+
+    println("addLetter after when $letter")
+
+    val newWordList = ws.wordList.toMutableList()
+    newWordList[ws.cursorRow] = newWord
+
+    wordleStateFlow.value = ws.copy(
+      wordList = newWordList,
+//      cursorRow = newCursorRow
+    )
   }
 
 
   // so i need to improve my kotlin skills
-//  fun addLetter(letter: Char) {
+//  fun addLetterXXXX(letter: Char) {
 //    println("addLetter $letter")
-//    val ws = wordleStateFlow.value
 //
 //    val word = ws.wordList[ws.cursorRow].toCharArray()
 //    word[ws.cursorCol] = letter
