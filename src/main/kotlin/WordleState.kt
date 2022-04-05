a8bf80277c3ab661fdc86d20a87d688b35220c1c
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-05 16:51:39 +1000

    .

--- src/main/kotlin/WordleState.kt
+++ src/main/kotlin/WordleState.kt
@@ -3,17 +3,14 @@
 // or WordleState holds mutablestate objects
 // or make WordleState var mutable state
 
-
 // is it ok to stick all state in single data class?
 // probably, doitwrong
 
 data class WordleState(
-//  val wordList: List<String> = listOf("00000", "00000", "00000", "00000", "00000", "00000"),
   val wordList: List<String> = listOf("", "", "", "", "", ""),
   val cursorRow: Int = 0,
-//  val cursorCol: Int = 0
-
   val solution: String = "TOMMY",
+
   // better off to derive this state
 //  val typedLettersPositionMatch: Set<Char> = setOf(),
 //  val typedLettersMatch: Set<Char> = setOf()
