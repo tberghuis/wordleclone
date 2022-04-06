d91d53142f5e7887d00fb7e609d7a0c9b59c1f45
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-07 05:27:28 +1000

    .

--- src/main/kotlin/generate/generate_valid_word_list.kt
+++ src/main/kotlin/generate/generate_valid_word_list.kt
@@ -9,23 +9,41 @@ import java.io.File
 // is there anyway to call getResource from main()
 class GenWordListKt {
   init {
-    val validWords = javaClass.classLoader.getResource("valid_words.txt")?.readText()?.lines()
-//    println("validWords $validWords")
-    writeListToFile(validWords!!)
+//    val validWords = javaClass.classLoader.getResource("valid_words.txt")?.readText()?.lines()
+//    writeListToFile(validWords!!)
+
+    genWords("valid_words.txt", "VALID_WORDS", "src/main/kotlin/valid_words.kt")
+    genWords("top_words.txt", "TOP_WORDS", "src/main/kotlin/top_words.kt")
   }
 
-  private fun writeListToFile(validWords: List<String>) {
-    var sl2 = ""
-    validWords.forEach {
-      sl2 += "\"$it\","
-    }
-    val wholeFile = """
-      val VALID_WORDS = listOf(
+  private fun genWords(resource: String, constant: String, ktFile: String) {
+    val words = javaClass.classLoader.getResource(resource)?.readText()?.lines()
+    words?.let {
+      var sl2 = ""
+      words.forEach {
+        sl2 += "\"$it\","
+      }
+      val wholeFile = """
+      val $constant = listOf(
         $sl2
       )
       """.trimIndent()
-    File("src/main/kotlin/valid_words.kt").writeText(wholeFile)
+      File(ktFile).writeText(wholeFile)
+    }
   }
+
+//  private fun writeListToFile(validWords: List<String>) {
+//    var sl2 = ""
+//    validWords.forEach {
+//      sl2 += "\"$it\","
+//    }
+//    val wholeFile = """
+//      val VALID_WORDS = listOf(
+//        $sl2
+//      )
+//      """.trimIndent()
+//    File("src/main/kotlin/valid_words.kt").writeText(wholeFile)
+//  }
 }
 
 fun main() {
---
+++ src/main/kotlin/top_words.kt
@@ -0,0 +1,3 @@
+val TOP_WORDS = listOf(
+  "WORDS","HELLO",
+)
